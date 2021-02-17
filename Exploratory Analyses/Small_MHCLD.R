79673df0e650ca459ecef4806fed7412c4ce5ab8
bryson716 <brysomor716@gmail.com>
2021-02-16 19:42:45 -0800

    create bar graph rough draft

--- Exploratory Analyses/Small_MHCLD.R
+++ Exploratory Analyses/Small_MHCLD.R
@@ -4,5 +4,5 @@ mhcld_data <- read.csv("MHCLD_PUF_2018.csv")
 mhcld_data <- mhcld_data[!duplicated(mhcld_data$CASEID), ]
 smaller <- mhcld_data %>% 
   filter(AGE != -9, EDUC != -9, ETHNIC != -9, RACE != -9, MH1 != -9) %>%
-  select(AGE:GENDER, IJSSERVICE, NUMMHS:ODDFLG, SCHIZOFLG, ALCSUBFLG, STATEFIP:REGION) %>%
+  select(AGE:GENDER, MH1, NUMMHS:ODDFLG, SCHIZOFLG, ALCSUBFLG, STATEFIP:REGION) %>%
   write.csv(file = "~/Desktop/Small_MHCLD.csv", row.names = F)
