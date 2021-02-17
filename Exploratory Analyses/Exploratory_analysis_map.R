b9fa5d0b7048ca72d58ca5cc2c9859e32a77d298
carolinelow <clow14@uw.edu>
2021-02-17 00:07:12 -0800

    add map code

---
+++ Exploratory Analyses/Exploratory_analysis_map.R
@@ -0,0 +1,54 @@
+library(tidyverse)
+library("dplyr")
+library("ggplot2") 
+library("maps")
+library("mapproj")
+#load data
+mental_health_facilities <- read.csv("nmhss-puf-2018-csv.csv")
+
+#load state map data
+state_shapes <- map_data("state") %>%
+  mutate(name = toupper(region))
+abbreviations <- data.frame(name = state.name, abb= state.abb) %>%
+  mutate(name = toupper(name))
+state_map <- left_join(state_shapes, abbreviations, by = "name") %>%
+  mutate(LST = abb)
+
+#filter facility data
+facility_data <- select(mental_health_facilities, MHINTAKE, LST, FACILITYTYPE)
+
+#facilities with mental health intake
+mh_intake <- facility_data %>%
+  filter(MHINTAKE == 1)
+
+#aggregate mh intake
+agg_mh_intake <- aggregate(mh_intake['MHINTAKE'], by=mh_intake['LST'], sum)
+
+#join state and mh intake data
+state_mh_intake <- left_join(state_map, agg_mh_intake, by = "LST")
+
+#make map
+blank_theme <- theme_bw() +
+  theme(
+    axis.line = element_blank(),
+    axis.text = element_blank(),
+    axis.ticks = element_blank(),
+    plot.background = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.border = element_blank(),
+  )
+#make actual map
+mh_intake_map <- ggplot(state_mh_intake) +
+  geom_polygon(
+    mapping = aes(x = long, y = lat, group = group, fill = MHINTAKE),
+    color = "gray", size = 0.3
+  ) +
+  coord_map() +
+  scale_fill_continuous(limits = c(0, max(state_mh_intake$MHINTAKE)), 
+                        na.value = "white", low = "red", high = "blue") +
+  labs(title = "Number of Mental Health Facilities in Each State, 2018") +
+  xlab("Longitude") +
+  ylab("Latitude") +
+  labs(fill = "Number of Facilities") +
+  blank_theme
