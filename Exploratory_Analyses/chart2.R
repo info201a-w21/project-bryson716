7c47f9ce19aca844d1927449d21610b7d9b51c0d
Nina Tran <ninatktran@gmail.com>
2021-02-17 18:36:27 -0800

    Add a _ to the Exploratory Analyses folder

---
+++ Exploratory_Analyses/chart2.R
@@ -0,0 +1,57 @@
+# Load the packages 
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
+library(RColorBrewer) 
+
+# create color scheme
+color_scheme <- c( "powderblue", "paleturquoise2", "darkseagreen1", "palegreen", 
+                   "turquoise", "mediumaquamarine", "mediumseagreen", "mediumpurple",
+                   "slateblue1", "slateblue4", "steelblue4", "royalblue4", "navy")
+
+# Load the Unemployment and Mental Illness Survey data into a variable  
+df <- read.csv("data/unemploymentdata.csv", stringsAsFactors = FALSE)
+
+# Rename specific column names ("I.am.currently.employed.at.least.part.time"
+# and ("I.identify.as.having.a.mental.illness") to Employed and Have_MentalIllness
+colnames(df)[1] <- "Curr_Employed" 
+colnames(df)[2] <- "Have_MentalIllness"
+
+# How many people are employed? 
+employment <- df %>% 
+  summarize(
+    employed = sum(Curr_Employed == 1), 
+    unemployed = sum(Curr_Employed == 0), 
+  )
+employment
+
+# How many people identify as having a mental illness? 
+mentalillness <- df %>% 
+  summarize(
+    yes = sum(Have_MentalIllness == 1), 
+    no = sum(Have_MentalIllness == 0), 
+  )
+mentalillness
+
+blank_theme <- theme_bw() +
+  theme(
+    axis.line = element_blank(),        # remove axis lines
+    plot.background = element_blank(),  # remove gray background
+    panel.grid.major = element_blank(), # remove major grid lines
+    panel.grid.minor = element_blank(), # remove minor grid lines
+    panel.border = element_blank()      # remove border around plot
+  )
+
+stacked_barchart <- ggplot(df, aes(fill = Gender, y = Have_MentalIllness, x = Curr_Employed)) + 
+  geom_bar(position = "stack", stat="identity") +
+  scale_fill_manual(values = c("mediumpurple", "turquoise"))  +
+  scale_x_continuous(breaks = seq(0, 1.0, by=1), 
+                     labels = c("Unemployed", "Employed")) +
+  blank_theme + 
+  labs(x = "Employment", 
+       y = "Identify as Having a Mental Illness", 
+       title = "The Interesections of Unemployment, Gender, and Mental Health") +
+  theme(plot.title = element_text(hjust = 0.5))  
+
+print(stacked_barchart)
+
---
+++ Exploratory_Analyses/chart3.R
@@ -0,0 +1,58 @@
+library(tidyverse)
+library(stringr)
+library(ggrepel)
+data <- read.csv("data/Small_MHCLD.csv", stringsAsFactors = FALSE)
+data <- select(data, ETHNIC, RACE, MH1)
+
+# create color scheme
+color_scheme <- c( "powderblue", "paleturquoise2", "darkseagreen1", "palegreen", 
+                   "turquoise", "mediumaquamarine", "mediumseagreen", "mediumpurple",
+                   "slateblue1", "slateblue4", "steelblue4", "royalblue4", "navy")
+
+# edit text
+data$RACE <- data$RACE %>%
+  str_replace_all("1", "American Indian/Alaska Native") %>%
+  str_replace_all("2", "Asian") %>%
+  str_replace_all("3", "Black/African American") %>%
+  str_replace_all("4", "Native Hawaiian/Pacific Islander") %>%
+  str_replace_all("5", "White") %>%
+  str_replace_all("6", "Multiracial/Other")
+
+data$MH1 <- data$MH1 %>%
+  str_replace_all("10", "Personality Disorder") %>%
+  str_replace_all("11", "Schizophrenia") %>%
+  str_replace_all("12", "Substance Abuse") %>%
+  str_replace_all("13", "Other") %>%
+  str_replace_all("1", "Trauma") %>%
+  str_replace_all("2", "Anxiety Disorder") %>%
+  str_replace_all("3", "ADD/ADHD") %>%
+  str_replace_all("4", "Conduct Disorder") %>%
+  str_replace_all("5", "Delirium/Dementia") %>%
+  str_replace_all("6", "Bipolar") %>%
+  str_replace_all("7", "Depression") %>%
+  str_replace_all("8", "ODD") %>%
+  str_replace_all("9", "PDD")
+
+# calculate proportions
+mental_illness <- data %>%
+  group_by(RACE, MH1) %>%
+  summarise("total_w_illness" = length(RACE))
+
+race_totals <- data %>%
+  group_by(RACE) %>%
+  summarise("total_race" = length(RACE))
+
+race_illness_prop <- left_join(mental_illness, race_totals, by = "RACE") %>%
+  mutate("prop" = total_w_illness/total_race)
+
+# create bar graph
+bar_graph <- ggplot(data = race_illness_prop) +
+  geom_col(mapping = aes(x = RACE, y = prop, fill = MH1),
+           position = "fill") +
+  scale_fill_manual(values = color_scheme) +
+  labs(title = "Racial Proportions of Mental Illness Diagnoses, 2018",
+       x = "Race",
+       y = "Total",
+       fill = "Mental Illness")
+print(bar_graph)
+
