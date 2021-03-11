994be2b6413876e1cdd3a57c8f705344a28f28c2
Nina Tran <ninatktran@gmail.com>
2021-03-11 07:51:50 -0800

    conclusion is complete

--- mental_health_app/app_server.R
+++ mental_health_app/app_server.R
@@ -1,6 +1,86 @@
 # Server 
+library(tidyverse)
+library(plotly)
+
+data_one <- read.csv("data/small_MHCLD.csv")
+data_two <- read.csv("data/nmhss-puf-2018-csv.csv")  
+
+# Clean Data --------------------------------------------------------------
+data_one$GENDER <- data_one$GENDER %>% 
+  str_replace_all("1", "Male") %>% 
+  str_replace_all("2", "Female") %>% 
+  str_replace_all("-9", "Unknown") 
+
+data_one$RACE <- data_one$RACE %>%
+  str_replace_all("1", "American Indian/Alaska Native") %>%
+  str_replace_all("2", "Asian") %>%
+  str_replace_all("3", "Black/African American") %>%
+  str_replace_all("4", "Native Hawaiian/Pacific Islander") %>%
+  str_replace_all("5", "White") %>%
+  str_replace_all("6", "Multiracial/Other")
+
+facility_data <- data_one %>% 
+  select(AGE, EDUC, RACE, ETHNIC, GENDER, MH1)
+
+
+# Set up plots,  graphs, etc.  --------------------------------------------
 
 server <- function(input, output) {
 
-    
+
+
+# Conclusion Plot ---------------------------------------------------------
+
+output$bar <- renderPlotly({
+  p_data <- facility_data %>%
+    filter(GENDER != "Unknown") %>% 
+    select(AGE, GENDER, RACE, MH1) %>% 
+    group_by(AGE, GENDER, RACE) %>% 
+    mutate(MH1 = recode(MH1,
+                        "1" = "Trauma-Related Disorders",
+                        "2" = "Anxiety Disorders",
+                        "3" = "ADD/ADHD",
+                        "4" = "Conduct Disorder",
+                        "5" = "Delirium/Dementia",
+                        "6" = "Bipolar Disorder",
+                        "7" = "Depressive Disorders",
+                        "8" = "Oppositional Defiant Disorder",
+                        "9" = "Pervasive Developmental Disorder",
+                        "10" = "Personality Disorders",
+                        "11" = "Schizophrenia",
+                        "12" = "Substance Abuse", 
+                        "13" = "Other")) %>% 
+    # n = the numbers of people who have specific mental illnesses
+    count(MH1) %>% 
+    # prop = the proportion rate of people with specific mental illnesses in 
+    # age and race groups 
+    mutate(PROP = round((n / sum(n) * 100), digits = 2)) %>% 
+    filter(PROP == max(PROP)) %>% 
+    arrange()
+  
+  categories <- cut(p_data$AGE, breaks = c(0, 2, 5, 13, Inf), 
+        labels = c("Children", "Youth", "Adult", "Senior")) 
+  
+  p_data <- data.frame(p_data, categories)
+  
+  q <- ggplot(p_data, aes(fill = MH1, y = PROP, x = RACE, color = GENDER)) + 
+    geom_bar(position="dodge", stat="identity") +
+    theme_minimal() + 
+    facet_wrap(~categories) + 
+    scale_y_continuous(breaks = seq(0, 60, 10)) + 
+    scale_fill_viridis(discrete = TRUE) + 
+    labs(fill = "Mental Illnesses", 
+         color = "", 
+         y = "Proporation Rate of People with Specific Mental Illnesses", 
+         title = "What age and race groups are most affected by specific mental illness?") +
+    theme(axis.title.y = element_blank(), 
+          axis.title.x = element_text(size = 10, vjust = 1), 
+          panel.border = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank()) + 
+    coord_flip()
+  
+  ggplotly(q)
+  
+})  
 }
--- mental_health_app/app_ui.R
+++ mental_health_app/app_ui.R
@@ -9,7 +9,7 @@ intro <- tabPanel("Introduction",
 
 # Interactive Page 1 ------------------------------------------------------
 
-viz_one <- tabPanel("(Insert a title)"
+viz_one <- tabPanel("Mental Health Facilities in America"
                     
                     )
 
@@ -23,10 +23,35 @@ viz_two <- tabPanel("(Insert a title)"
 # Conclusion Page --------------------------------------------------------------
 
 concl <- tabPanel("Conclusion", 
-                  titlePanel("Main Takeaways"), 
-                             style = "text-align: center;"
+                  titlePanel("Project Takeaways"), 
+                             style = "text-align: center;",
+                  p("Before this study, we sought to investigate 
+                    how mental illness is distributed in the U.S. 
+                    based on different demographic characteristics.  
+                    The results of our investigation show that 
+                    the most affected racial group in both genders 
+                    and age groups is", strong("American Indian/Alaska Native"), 
+                    "which is not too surprising, but this new understanding 
+                    should help address those issues regarding the 
+                    margarlinzed groups facing oppression."),
+                  p("We also found that depressive disorders are highly prevelant 
+                    in older age groups compared to younger age groups. Due to
+                    the stigma of mental health, society does not recognize
+                    the struggles of older people struggling with their 
+                    mental illnesses. In addition, children between the age of 0 
+                    and 14 are more likely to experience 
+                    trauma-related disorders more than other age groups."), 
+                  p("Unfortunately, our datasets did not show 
+                  whether they received treatment and services for 
+                  their mental illnesses. However, we learned a lot about those 
+                  demographics in connection to gender, race, and age and 
+                  challenged the research questions through discussion and 
+                  crticial thinking."),
+                  p("This information in the interactive chart shown below
+                  can be used to develop targetted interventions aimed at 
+                  improving mental health care towards the marginalized groups."),
+                  plotlyOutput("bar")
                   )
-
 
 ui <- navbarPage(theme = shinytheme("united"),
                  "Mental Health in the U.S.", 
