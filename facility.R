743577791528e0090d6c4ad9563f7a49e9ccce87
carolinelow <clow14@uw.edu>
2021-03-12 17:36:24 -0800

    add facility page

---
+++ facility.R
@@ -0,0 +1,115 @@
+library(shiny)
+library(tidyverse)
+library(plotly)
+library(countrycode)
+library(knitr)
+library(stringr)
+library(rsconnect)
+library(maps)
+library(dplyr)
+mental_health <- read.csv("data/nmhss-puf-2018-csv.csv")
+colorscheme <- c("#E69F00", "#56B4E9", "#009E73", 
+                 "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+mental_health_facilities <- full_join(mental_health, state_populations,
+  by = "LST"
+)
+state_shapes <- map_data("state") %>%
+  mutate(name = toupper(region))
+abbreviations <- data.frame(name = state.name, abb = state.abb) %>%
+  mutate(name = toupper(name))
+state_map <- left_join(state_shapes, abbreviations, by = "name") %>%
+  mutate(LST = abb)
+psych <- mental_health_facilities %>%
+  select(CASEID, LST, FACILITYTYPE) %>%
+  filter(FACILITYTYPE == 1)
+agg_psych <- aggregate(psych["FACILITYTYPE"], by = psych["LST"], sum) %>%
+  mutate(number = FACILITYTYPE)
+final_psych <- left_join(psych, agg_psych, by = "LST")
+total <- mental_health_facilities %>%
+  select(CASEID, LST, MHINTAKE) %>%
+  filter(MHINTAKE == 1)
+agg_total <- aggregate(total["MHINTAKE"], by = total["LST"], sum) %>%
+  mutate(number = MHINTAKE)
+final_total <- left_join(total, agg_total, by = "LST")
+separate <- mental_health_facilities %>%
+  select(CASEID, LST, FACILITYTYPE) %>%
+  filter(FACILITYTYPE == 2)
+agg_separate <- aggregate(separate["FACILITYTYPE"], by = separate["LST"],
+                          sum) %>%
+  mutate(number = FACILITYTYPE / 2)
+final_separate <- left_join(separate, agg_separate, by = "LST")
+res_child <- mental_health_facilities %>%
+  select(CASEID, LST, FACILITYTYPE) %>%
+  filter(FACILITYTYPE == 3)
+agg_res_child <- aggregate(res_child["FACILITYTYPE"], by = res_child["LST"],
+                           sum) %>%
+  mutate(number = FACILITYTYPE / 3)
+final_res_child <- left_join(res_child, agg_res_child, by = "LST")
+res_adult <- mental_health_facilities %>%
+  select(CASEID, LST, FACILITYTYPE) %>%
+  filter(FACILITYTYPE == 4)
+agg_res_adult <- aggregate(res_adult["FACILITYTYPE"], by = res_adult["LST"],
+                           sum) %>%
+  mutate(number = FACILITYTYPE / 4)
+final_res_adult <- left_join(res_adult, agg_res_adult, by = "LST")
+other_res <- mental_health_facilities %>%
+  select(CASEID, LST, FACILITYTYPE) %>%
+  filter(FACILITYTYPE == 5)
+agg_other_res <- aggregate(other_res["FACILITYTYPE"],
+  by = other_res["LST"],
+  sum
+) %>%
+  mutate(number = FACILITYTYPE / 5)
+final_other_res <- left_join(other_res, agg_other_res, by = "LST")
+vet <- mental_health_facilities %>%
+  select(CASEID, LST, FACILITYTYPE) %>%
+  filter(FACILITYTYPE == 6)
+agg_vet <- aggregate(vet["FACILITYTYPE"], by = vet["LST"], sum) %>%
+  mutate(number = FACILITYTYPE / 6)
+final_vet <- left_join(vet, agg_vet, by = "LST")
+com <- mental_health_facilities %>%
+  select(CASEID, LST, FACILITYTYPE) %>%
+  filter(FACILITYTYPE == 7)
+agg_com <- aggregate(com["FACILITYTYPE"], by = com["LST"], sum) %>%
+  mutate(number = FACILITYTYPE / 7)
+final_com <- left_join(com, agg_com, by = "LST")
+partial <- mental_health_facilities %>%
+  select(CASEID, LST, FACILITYTYPE) %>%
+  filter(FACILITYTYPE == 8)
+agg_partial <- aggregate(partial["FACILITYTYPE"], by = partial["LST"], sum) %>%
+  mutate(number = FACILITYTYPE / 8)
+final_partial <- left_join(partial, agg_partial, by = "LST")
+outpatient <- mental_health_facilities %>%
+  select(CASEID, LST, FACILITYTYPE) %>%
+  filter(FACILITYTYPE == 9)
+agg_outpatient <- aggregate(outpatient["FACILITYTYPE"],
+  by = outpatient["LST"],
+  sum
+) %>%
+  mutate(number = FACILITYTYPE / 9)
+final_outpatient <- left_join(outpatient, agg_outpatient, by = "LST")
+multi <- mental_health_facilities %>%
+  select(CASEID, LST, FACILITYTYPE) %>%
+  filter(FACILITYTYPE == 10)
+agg_multi <- aggregate(multi["FACILITYTYPE"], by = multi["LST"], sum) %>%
+  mutate(number = FACILITYTYPE / 10)
+final_multi <- left_join(multi, agg_multi, by = "LST")
+other <- mental_health_facilities %>%
+  select(CASEID, LST, FACILITYTYPE) %>%
+  filter(FACILITYTYPE == 11)
+agg_other <- aggregate(other["FACILITYTYPE"], by = other["LST"], sum) %>%
+  mutate(number = FACILITYTYPE / 11)
+final_other <- left_join(other, agg_other, by = "LST")
+blank_theme <- theme_bw() +
+  theme(
+    axis.line = element_blank(),
+    axis.text = element_blank(),
+    axis.ticks = element_blank(),
+    axis.title = element_blank(),
+    plot.background = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.border = element_blank(),
+  )
+age_data <- mental_health %>%
+  select(CASEID, LST, CHILDAD, ADOLES, YOUNGADULTS, ADULT, SENIORS)
---
+++ facility_app.R
@@ -0,0 +1,190 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+library(plotly)
+library(countrycode)
+library(knitr)
+library(stringr)
+library(rsconnect)
+library(maps)
+library(dplyr)
+source("facility.R")
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+    titlePanel("Number of Mental Health Facilities in Each State"),
+    sidebarLayout(
+        sidebarPanel(
+            selectInput("facilities",
+                        label = h3("Type of Facility"),
+                                   c("Total" = "Total Mental Health Facilities",
+                                     "Psychiatric Hospital" =
+                                         "Psychiatric Hospitals",
+                                     "Separate inpatient psychiatric unit of a
+                                     general hospital" = "Separate Inpatient
+                                     Psychiatric Units of a General Hospital",
+                                     "Residential treatment center for
+                                     children" = "Residential Treatment Centers
+                                     for Children",
+                                     "Residential treatment center for adults"
+                                     = "Residential Treatment Centers for
+                                     Adults",
+                                     "Other type of residential treatment
+                                     facility" = "Other Types of Residential
+                                     Facilities",
+                                     "Veterans Administration medical center
+                                     (VAMC)" = "Veterans Administration Medical
+                                     Centers",
+                                     "Community mental health center (CMHC)"
+                                     = "Community Mental Health Centers",
+                                     "Partial hospitalization/day treatment
+                                     facility" = "Partial Hospitalization/Day
+                                     Treatment Facilities",
+                                     "Outpatient mental health facility" =
+                                         "Outpatient Mental Health Facilities",
+                                     "Multi-setting mental health facility" =
+                                         "Multi-setting Mental Health
+                                     Facilities",
+                                     "Other" = "Other")),
+           selectInput(inputId = "state",
+                        label = h3("Select a State"),
+                        c("Alabama (AL)" = "AL", "Alaska (AK)" = "AK",
+                        "Arizona (AZ)" = "AZ", "Arkansas (AR)" = "AR",
+                        "California (CA)" = "CA", "Colorado (CO)" = "CO",
+                        "Connecticut (CT)" = "CT", "Delaware (DE)" = "DE",
+                        "Florida (FL)" = "FL", "Georgia (GA)" = "GA",
+                        "Hawaii (HI)" = "HI", "Idaho (ID)" = "ID",
+                        "Illinois (IL)" = "IL", "Indiana (IN)" = "IN",
+                        "Iowa (IA)" = "IA", "Kansas (KS)" = "KS",
+                        "Kentucky (KY)" = "KY", "Louisiana (LA)" = "LA",
+                        "Maine (ME)" = "ME", "Maryland (MD)" = "MD",
+                        "Massachusetts (MA)" = "MA", "Michigan (MI)" = "MI",
+                        "Minnesota (MN)" = "MN", "Mississippi (MS)" = "MS",
+                        "Missouri (MO)" = "MO", "Montana (MT)" = "MT",
+                        "Nebraska (NE)" = "NE", "Nevada (NV)" = "NV",
+                        "New Hampshire (NH)" = "NH", "New Jersey (NJ)" = "NJ",
+                        "New Mexico (NM)" = "NM", "New York (NY)" = "NY",
+                        "North Carolina (NC)" = "NC",
+                         "North Dakota (ND)" = "ND",
+                        "Ohio (OH)" = "OH", "Oklahoma (OK)" = "OK",
+                        "Oregon (OR)" = "OR", "Pennsylvania (PA)" = "PA",
+                        "Rhode Island (RI)" = "RI",
+                         "South Carolina (SC)" = "SC",
+                        "South Dakota (SD)" = "SD", "Tennessee (TN)" = "TN",
+                        "Texas (TX)" = "TX", "Utah (UT)" = "UT",
+                        "Vermont (VT)" = "VT", "Virginia (VA)" = "VA",
+                        "Washington (WA)" = "WA", "West Virginia (WV)" = "WV",
+                       "Wisconsin (WI)" = "WI", "Wyoming (WY)" = "WY"
+                        ))),
+        mainPanel(
+            plotlyOutput("facility_map"),
+            plotlyOutput("facility_graph")
+    ),
+),
+)
+server <- function(input, output) {
+    facilities_input <- reactive({
+        if (input$facilities == "Total Mental Health Facilities") {
+            dataset <- final_total
+        }
+        if (input$facilities == "Psychiatric Hospitals") {
+            dataset <- final_psych
+        }
+        if (input$facilities == "Separate Inpatient Psychiatric Units of a
+            General Hospital") {
+            dataset <- final_separate
+        }
+        if (input$facilities == "Residential Treatment Centers for Children") {
+            dataset <- final_res_child
+        }
+        if (input$facilities == "Residential Treatment Centers for Adults") {
+            dataset <- final_res_adult
+        }
+        if (input$facilities == "Other Types of Residential Facilities") {
+            dataset <- final_other_res
+        }
+        if (input$facilities == "Veterans Administration Medical Centers") {
+            dataset <- final_vet
+        }
+        if (input$facilities == "Community Mental Health Centers") {
+            dataset <- final_com
+        }
+        if (input$facilities == "Partial Hospitalization/Day Treatment
+            Facilities") {
+            dataset <- final_partial
+        }
+        if (input$facilities == "Outpatient Mental Health Facilities") {
+            dataset <- final_outpatient
+        }
+        if (input$facilities == "Multi-setting Mental Health Facilities") {
+            dataset <- final_multi
+        }
+        else if (input$facilities == "Other") {
+            dataset <- final_other
+        }
+        return(dataset)
+    })
+    output$facility_map <- renderPlotly({
+        {facilities_input() %>%
+                full_join(state_map, by = "LST") %>%
+                select(CASEID, long, lat, group, order, LST, number) %>%
+                distinct(order, .keep_all = TRUE) %>%
+                ggplot() +
+                geom_polygon(
+                    mapping = aes(x = long, y = lat, group = group,
+                                  LST = LST, fill = number,
+                                  text = paste("State:", LST,
+                                               "<br>Number of Facilities:",
+                                               number)),
+                    color = "gray", size = 0.3
+                ) +
+                coord_map() +
+                scale_fill_continuous(limits = c(0,
+                                                max(facilities_input()$number)),
+                                      na.value = "white", low = "#56B4E9",
+                                      high = "#D55E00") +
+                labs(title = str_wrap(paste("Number of", input$facilities,
+                                            "in Each State, 2018"), 60)) +
+                labs(fill = "Number of Facilities") +
+                blank_theme
+    } %>%
+            ggplotly(tooltip = "text")
+        })
+    output$facility_graph <- renderPlotly({
+        level_order <- c("CHILDAD", "ADOLES", "YOUNGADULTS", "ADULT", "SENIORS")
+        {facilities_input() %>%
+                left_join(age_data, by = "CASEID") %>%
+                filter(LST.x == input$state) %>%
+                select(CASEID, CHILDAD, ADOLES, YOUNGADULTS, ADULT, SENIORS,
+                       LST.x) %>%
+                na.omit() %>%
+                distinct(CASEID, .keep_all = TRUE) %>%
+                select(CHILDAD, ADOLES, YOUNGADULTS, ADULT, SENIORS) %>%
+                summarise_each(funs = sum) %>%
+                gather() %>%
+                ggplot() +
+                geom_col(aes(x = factor(key, level = level_order), y = value,
+                             fill = key,
+                             text = paste("Number of Facilities:", value))) +
+                scale_fill_manual(values = colorscheme, name = "Age Group") +
+                scale_x_discrete(labels = c("0-12", "13-17", "18-25", "26-64",
+                                          "65+", labels = c("0-12", "13-17",
+                                                            "18-25", "26-64",
+                                                            "65+"))) +
+                labs(title = str_wrap(paste("Number of", input$facilities,
+                                            "in", input$state, ", 2018"), 60)) +
+                xlab("Ages Accepted") +
+                ylab("Number of Facilities")
+            } %>%
+            ggplotly(tooltip = "text")
+    })
+}
+# Run the application
+shinyApp(ui = ui, server = server)
